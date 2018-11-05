b4669885d817c123a0519a47ae16862ae63e8a96
Carlos <djkrlosmix1@gmail.com>
2018-11-04 19:39:10 -0500

    listo el webpack dev

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+//components
+import Media from "./src/playlist/components/media";
+
+const app = document.getElementById("app")
+
+render(<Media />,app)
---
+++ src/playlist/components/media.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+
+class Media extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+            <img 
+                src="" 
+                alt="" 
+                srcset=""
+                width = {260}
+                height = {160}
+            />
+            <h3>Aprende React!</h3>
+            <p>Carlos csz</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Media;
